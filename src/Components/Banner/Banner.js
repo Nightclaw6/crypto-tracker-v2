68eac2d81bdab9d72558990ef95c643e6a968141
Nightclaw6 <sclaudiu82@yahoo.com>
2022-05-25 13:25:57 -0400

    added coin page details

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Container, makeStyles } from "@material-ui/core";
+import { Container, makeStyles, Typography } from "@material-ui/core";
+import Carousel from './Carousel';
 
 const useStyles = makeStyles( () => ({
     banner: {
@@ -11,6 +12,13 @@ const useStyles = makeStyles( () => ({
         flexDirection: "column",
         paddingTop: 25,
         justifyContent: "space-around",
+    },
+    tagline: {
+      display: "flex",
+      height: "40%",
+      flexDirection: "column",
+      justifyContent: "center",
+      textAlign: "center",
     }
   }));
 
@@ -18,7 +26,23 @@ const Banner = () => {
   const classes = useStyles();
   return <div className={classes.banner}>
       <Container className={classes.bannerContent}>
-
+        <div className={classes.tagline}>
+          <Typography variant="h2" style={{
+            fontWeight: "bold",
+            marginBottom: 15,
+            fontFamily: "Montserrat",
+          }}>
+            Crypto Tracker
+          </Typography>
+          <Typography variant="subtitle2" style={{
+            color:"darkgrey",
+            textTransform: "capitalize",
+            fontFamily: "Montserrat",
+          }}>
+            Get all the information about your favourite crypto currency.
+          </Typography>
+        </div>
+        <Carousel />
       </Container>
 
   </div> 
